4a147ebcd038d11845468b794c648dcd12115996
kushal sharma <mrkushalsharma@gmail.com>
2019-08-01 14:42:18 +0545

    resized cabbage

--- compress.py
+++ compress.py
@@ -1,12 +1,11 @@
-# from PIL import Image
-# # My image is a 200x374 jpeg that is 102kb large
-# #foo = Image.open("path\\to\\image.jpg")
-# foo = Image.open("33.jpg")
-# #foo.size(100, 100)
-# # I downsize the image with an ANTIALIAS filter (gives the highest quality)
-# foo = foo.resize((100, 100), Image.ANTIALIAS)
-# #foo.save("c1.jpg")
-# # The saved downsized image size is 24.8kb
-# foo.save("c3.jpg", optimize=False, quality=1000)
-# Write Python code here 
-# import the necessary packages 
+import os
+from datetime import datetime
+from PIL import Image
+
+for image_file_name in os.listdir('cabbage/'):
+    if image_file_name.endswith(".jpg"):
+        now = datetime.now().strftime('%Y%m%d-%H%M%S-%f')
+
+        foo = Image.open('cabbage/'+image_file_name)
+        foo = foo.resize((100, 100), Image.ANTIALIAS)
+        foo.save('resized/cabbage/' + now + '.jpg', optimize=True, quality=100)
