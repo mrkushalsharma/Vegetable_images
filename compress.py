db5db3551b3b00a22ce9c0b1102c6ae4714607da
kushal sharma <mrkushalsharma@gmail.com>
2019-08-01 12:20:07 +0545

    garlic resized

--- compress.py
+++ compress.py
@@ -2,10 +2,10 @@ import os
 from datetime import datetime
 from PIL import Image
 
-for image_file_name in os.listdir('cabbage/'):
+for image_file_name in os.listdir('garlic/'):
     if image_file_name.endswith(".jpg"):
         now = datetime.now().strftime('%Y%m%d-%H%M%S-%f')
 
-        foo = Image.open('cabbage/'+image_file_name)
+        foo = Image.open('garlic/'+image_file_name)
         foo = foo.resize((100, 100), Image.ANTIALIAS)
-        foo.save('resized/cabbage/' + now + '.jpg', optimize=True, quality=100)
+        foo.save('resized/garlic/' + now + '.jpg', optimize=True, quality=100)
